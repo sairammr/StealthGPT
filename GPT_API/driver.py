423c63ae03d6e53c971e6e412ce9d37a0a950d97
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-06-14 14:46:57 +0530

    fixed launch

--- GPT_API/driver.py
+++ GPT_API/driver.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 def create_driver():
     try:
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        
         chrome_options.add_argument("--remote-debugging-port=9222")
         driver = webdriver.Chrome(options=chrome_options)
         return driver
--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from GPT_API import prompt
 from GPT_API import PlatformBasedDriver
 
 from flask import Flask, jsonify, request, json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__)
-driver = None
+PlatformBasedDriver.debuggingModeLauncher()
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "localhost:9222")
+driver = webdriver.Chrome(options=chrome_options)
 
-def initialize_driver():
-    PlatformBasedDriver.debuggingModeLauncher()
-    global driver
-    # Create the WebDriver instance if it's not already initialized
-    if driver is None:
-        driver = gpt_driver.create_driver()
-        driver.get("https://chatgpt.com/")
+
 @app.route("/getSendPrompt", methods=['POST'])
 def stealthGPT():
     json_prompt = request.get_json()
@@ -21,7 +20,6 @@ def stealthGPT():
         received_prompt = json.loads(json_prompt)
         #driver = gpt_driver.create_driver()
         #driver.get("https://chatgpt.com/")
-        initialize_driver()
         result = prompt.promptEnter(driver, received_prompt)
         if result:
             return jsonify({"message": result}), 200
